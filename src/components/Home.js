49ec27bb23637b089ae548aed5bfc201466dcbdc
Joe Smith <joeycrushed@gmail.com>
2021-09-28 12:37:23 +0100

    key frame animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion'
 
+const buttonVariants = {
+  
+  hover: {
+      scale: [1, 1.1, 1, 1.1, 1, 1.1, 1, 1.1, 1],
+      textShadow: "0px 0px 8px rgb(255,255,255)",
+      boxShadow: "0px 0px 8px rgb(255,255,255)"
+  }
+}
+
 const Home = () => {
   return (
     <motion.div className="home container"
@@ -14,11 +23,8 @@ const Home = () => {
       </h2>
       <Link to="/base">
         <motion.button 
-          whileHover={{ 
-            scale: 1.1,
-            textShadow: "0px 0px 8px rgb(255,255,255)",
-            boxShadow: "0px 0px 8px rgb(255,255,255) "
-            }}
+        variants={buttonVariants}
+          whileHover="hover"
         >
           Create Your Pizza
         </motion.button>
