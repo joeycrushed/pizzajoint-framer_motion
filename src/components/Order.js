09b39622f75eb1b0e25ec3bb3a58a9ed40b37da9
Joe Smith <joeycrushed@gmail.com>
2021-09-28 15:48:51 +0100

    modal

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion'
 
 const containerVariants = {
@@ -32,8 +32,12 @@ const childVariants = {
   }
 }
 
-const Order = ({ pizza }) => {
-
+const Order = ({ pizza, setShowModal }) => {
+  useEffect(() => {
+    setTimeout(() => {
+      setShowModal(true)
+    }, 5000)
+  }, [setShowModal])
 
   return (
     <motion.div className="container order"
@@ -43,12 +47,10 @@ const Order = ({ pizza }) => {
       exit="exit"
     >
           <h2>Thank you for your order :)</h2>
-    
       <motion.p variants={childVariants}>You ordered a {pizza.base} pizza with:</motion.p>
       <motion.div variants={childVariants}>
         {pizza.toppings.map(topping => <div key={topping}>{topping}</div>)}
       </motion.div>
-      
     </motion.div>
   )
 }
